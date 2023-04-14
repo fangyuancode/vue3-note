32c66ef6413136a2fd76f24084b7f88d9706038b
fangyuancode <3258552734@qq.com>
2023-04-14 15:25:24 +0800

    学习笔记

---
+++ 01-Vue基础知识学习/08-Vue的阶段案例/data/data.js
@@ -0,0 +1,39 @@
+const books = [
+  {
+    id: 1,
+    name: '《算法导论》',
+    date: '2023-9',
+    price: 85.00,
+    count: 1
+  },
+  {
+    id: 2,
+    name: '《UNIX编程艺术》',
+    date: '2023-2',
+    price: 59.00,
+    count: 1
+  },
+  {
+    id: 3,
+    name: '《编程珠玑》',
+    date: '2023-10',
+    price: 39.00,
+    count: 1
+  },
+  {
+    id: 4,
+    name: '《代码大全》',
+    date: '2023-3',
+    price: 128.00,
+    count: 1
+  },
+  {
+    id:5,
+    name: '《javascript程序设计》',
+    date: '2023-3',
+    price: 128.00,
+    count: 1
+  },
+]
+
+
---
+++ 10-project/3-4project/demo.js
@@ -0,0 +1,14 @@
+let arr = {
+    cite1: {
+        title: "上海",
+        cites: {
+        }
+    },
+    cite2: {
+        title: "深圳",
+        cites: {
+        }
+    }
+}
+const current = arr["cite2"];
+console.log(current);
